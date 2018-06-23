b394c206ecb5e68833750df395b87ddde90c8bdf
Arjun S Bharadwaj <arjun.s.waj@gmail.com>
2018-06-23 18:49:19 +0530

    Adding IO DSL

---
+++ src/main/scala/com/asb/free/dsl/IODSL.scala
@@ -0,0 +1,36 @@
+package com.asb.free.dsl
+
+import java.io.{BufferedReader, Closeable}
+import java.nio.file.Path
+
+import cats.InjectK
+import cats.free.Free
+
+object IODSL {
+
+  sealed trait IOAction[A]
+
+  case class GetBufferedReader(path: Path) extends IOAction[BufferedReader]
+  case class ReadLine(bufferedReader: BufferedReader) extends IOAction[String]
+  case class Close(closeable: Closeable) extends IOAction[Unit]
+
+  class IOActions[F[_]](implicit I: InjectK[IOAction, F]) {
+
+    type IOActionF[A] = Free[IOAction, A]
+
+    def getBufferedReader(path: Path): IOActionF[BufferedReader] =
+      Free.inject(GetBufferedReader(path))
+
+    def readLine(bufferedReader: BufferedReader): IOActionF[String] =
+      Free.inject(ReadLine(bufferedReader))
+
+    def close(closeable: Closeable): IOActionF[Unit] =
+      Free.inject(Close(closeable))
+
+  }
+
+  object IOActions {
+    def apply[F[_]](implicit I: InjectK[IOAction, F]): IOActions[F] = new IOActions[F]
+  }
+
+}
