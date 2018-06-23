1443e1d8d565e785554d65d047af7fc51bd6d383
Arjun S Bharadwaj <arjun.s.waj@gmail.com>
2018-06-23 19:31:26 +0530

    Updating the Program to use multiple DSLs

--- src/main/scala/com/asb/free/WeatherProgram.scala
+++ src/main/scala/com/asb/free/WeatherProgram.scala
@@ -1,25 +1,50 @@
 package com.asb.free
 
+import cats.data.EitherK
 import cats.free.Free
 import cats.implicits._
-import com.asb.free.WeatherDSL._
-
-import scala.concurrent.Await
-import scala.concurrent.ExecutionContext.Implicits.global
-import scala.concurrent.duration._
+import com.asb.free.dsl.ConsoleDSL.{ConsoleAction, ConsoleActions}
+import com.asb.free.dsl.FileDSL.{FileAction, FileActions}
+import com.asb.free.dsl.GeoModelDSL.{GeoModelAction, GeoModelActions}
+import com.asb.free.dsl.IODSL.{IOAction, IOActions}
+import com.asb.free.dsl.NetworkDSL.{NetworkAction, NetworkActions}
+import com.asb.free.dsl.WeatherRequestUtilsDSL.{WeatherRequestAction, WeatherRequestActions}
+import com.asb.free.dsl.WeatherResponseUtilsDSL.{WeatherResponseAction, WeatherResponseActions}
 
 object WeatherProgram {
 
-  def getTemperatureByIp: Free[WeatherAction, Double] = for {
-    ip <- getIp
-    cityResponse <- getCityResponse(ip)
-    weather <- getWeather(cityResponse)
-    temperature <- getTemperature(weather)
+  type P1[A] = EitherK[ConsoleAction, FileAction, A]
+  type P2[A] = EitherK[IOAction, P1, A]
+  type P3[A] = EitherK[NetworkAction, P2, A]
+  type P4[A] = EitherK[GeoModelAction, P3, A]
+  type P5[A] = EitherK[WeatherRequestAction, P4, A]
+  type P[A] = EitherK[WeatherResponseAction, P5, A]
+
+  def getTemperatureByIp(implicit C: ConsoleActions[P], F: FileActions[P], I: IOActions[P],
+                         N: NetworkActions[P], G: GeoModelActions[P], WRQ: WeatherRequestActions[P],
+                         WRS: WeatherResponseActions[P]): Free[P, Double] = for {
+    ip <- C.readLine
+    cityResponse <- G.getCityResponse(ip)
+    filePath <- F.getFilePath("resources/weather.key")
+    reader <- I.getBufferedReader(filePath)
+    key <- WRQ.getKey(reader)
+    _ <- I.close(reader)
+    uri <- WRQ.getUrl(cityResponse.getLocation.getLatitude,
+      cityResponse.getLocation.getLongitude, key)
+    response <- N.doGet(uri)
+    temperature <- WRS.getTemperature(response)
   } yield temperature
 
   def main(args: Array[String]): Unit = {
-    val temperatureFuture = getTemperatureByIp.foldMap(WeatherInterpreter)
-    val temperature = Await.result(temperatureFuture, 5.seconds)
+    implicit val C: ConsoleActions[P] = ConsoleActions[P]
+    implicit val F: FileActions[P] = FileActions[P]
+    implicit val I: IOActions[P] = IOActions[P]
+    implicit val N: NetworkActions[P] = NetworkActions[P]
+    implicit val G: GeoModelActions[P] = GeoModelActions[P]
+    implicit val WRQ: WeatherRequestActions[P] = WeatherRequestActions[P]
+    implicit val WRS: WeatherResponseActions[P] = WeatherResponseActions[P]
+
+    val temperature = getTemperatureByIp
     println(temperature)
   }
 
