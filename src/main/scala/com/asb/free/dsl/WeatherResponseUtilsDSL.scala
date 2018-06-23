35397ba0dcf444759f728de3fa34c84f6557d914
Arjun S Bharadwaj <arjun.s.waj@gmail.com>
2018-06-23 19:04:46 +0530

    Adding Weather Response Util DSL

---
+++ src/main/scala/com/asb/free/dsl/WeatherResponseUtilsDSL.scala
@@ -0,0 +1,25 @@
+package com.asb.free.dsl
+
+import cats.InjectK
+import cats.free.Free
+
+object WeatherResponseUtilsDSL {
+
+  sealed trait WeatherResponseAction[A]
+
+  case class GetTemperature(response: String) extends WeatherResponseAction[Double]
+
+  class WeatherResponseActions[F[_]](implicit I: InjectK[WeatherResponseAction, F]) {
+
+    type WeatherResponseActionF[A] = Free[WeatherResponseAction, A]
+
+    def getTemperature(response: String): WeatherResponseActionF[Double] =
+      Free.inject(GetTemperature(response))
+
+  }
+
+  object WeatherResponseActions {
+    def apply[F[_]](implicit I: InjectK[WeatherResponseAction, F]): WeatherResponseActions[F] = new WeatherResponseActions[F]
+  }
+
+}
