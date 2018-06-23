495085b451bdf9431279cc3a7002e12a936850f2
Arjun S Bharadwaj <arjun.s.waj@gmail.com>
2018-06-23 16:45:04 +0530

    Making it composable

--- src/main/scala/com/asb/impure/GetWeather.scala
+++ src/main/scala/com/asb/impure/GetWeather.scala
@@ -5,6 +5,7 @@ import java.net.InetAddress
 import java.nio.file.{Files, Paths}
 
 import com.maxmind.geoip2.DatabaseReader
+import com.maxmind.geoip2.model.CityResponse
 import org.apache.http.client.fluent.Request
 import org.apache.http.client.utils.URIBuilder
 import org.json.JSONObject
@@ -12,54 +13,57 @@ import org.json.JSONObject
 object GetWeather {
 
   def main(args: Array[String]): Unit = {
+
+    val weather = Option.apply(getIP)
+      .map(getCityResponse)
+      .map(getWeather)
+      .map(getTemperature)
+      .getOrElse("")
+
+    println(weather)
+
+  }
+
+  def getIP: String = {
     val reader = new BufferedReader(new InputStreamReader(System.in))
     try {
-      val ipAddressStr = reader.readLine
-
-
-      val database = Paths.get("resources", "GeoLite2-City.mmdb").toFile
-      val weatherKey = Files.readAllLines(Paths.get("resources", "weather.key")).get(0)
-
-      // This creates the DatabaseReader object. To improve performance, reuse
-      // the object across lookups. The object is thread-safe.
-      val dbreader = new DatabaseReader.Builder(database).build
-      try {
-        val ipAddress = InetAddress.getByName(ipAddressStr)
-
-        // Replace "city" with the appropriate method for your database, e.g.,
-        // "country".
-        val response = dbreader.city(ipAddress)
-        val longitude = response.getLocation.getLongitude
-        val latitude = response.getLocation.getLatitude
-
-        println(s"Latitude: $latitude, Longitude: $longitude")
-
-
-        val builder = new URIBuilder
-        builder.setScheme("http")
-          .setHost("api.openweathermap.org")
-          .setPath("/data/2.5/weather")
-          .setParameter("lat", latitude.toString)
-          .setParameter("lon", longitude.toString)
-          .setParameter("appid", weatherKey)
-
-        val content = Request.Get(builder.build)
-          .connectTimeout(1000)
-          .socketTimeout(1000)
-          .execute().returnContent().asString()
-
-        val json = new JSONObject(content)
-        val temp = json.getJSONObject("main").getDouble("temp")
-        println(s"Temperature is $temp")
-
-      } finally {
-        dbreader.close()
-      }
-
+      reader.readLine
     } finally {
       reader.close()
     }
+  }
 
+  def getCityResponse(ipAddress: String): CityResponse = {
+    val database = Paths.get("resources", "GeoLite2-City.mmdb").toFile
+    val dbreader = new DatabaseReader.Builder(database).build
+    try {
+      dbreader.city(InetAddress.getByName(ipAddress))
+    } finally {
+      dbreader.close()
+    }
+  }
+
+  def getWeather(response: CityResponse): String = {
+    val weatherKey = Files.readAllLines(Paths.get("resources", "weather.key")).get(0)
+    val longitude = response.getLocation.getLongitude
+    val latitude = response.getLocation.getLatitude
+    val builder = new URIBuilder
+    builder.setScheme("http")
+      .setHost("api.openweathermap.org")
+      .setPath("/data/2.5/weather")
+      .setParameter("lat", latitude.toString)
+      .setParameter("lon", longitude.toString)
+      .setParameter("appid", weatherKey)
+
+    Request.Get(builder.build)
+      .connectTimeout(1000)
+      .socketTimeout(1000)
+      .execute().returnContent().asString()
+  }
+
+  def getTemperature(content: String): Double = {
+    val json = new JSONObject(content)
+    json.getJSONObject("main").getDouble("temp")
   }
 
 }
