cd28ba16c751bea9868b681cad9d3c0e5d838ab2
Arjun S Bharadwaj <arjun.s.waj@gmail.com>
2018-06-23 19:19:24 +0530

    Adding GetKey API to Weather Request Action DSL

--- src/main/scala/com/asb/free/dsl/WeatherRequestUtilsDSL.scala
+++ src/main/scala/com/asb/free/dsl/WeatherRequestUtilsDSL.scala
@@ -1,5 +1,6 @@
 package com.asb.free.dsl
 
+import java.io.BufferedReader
 import java.net.URI
 
 import cats.InjectK
@@ -9,11 +10,15 @@ object WeatherRequestUtilsDSL {
 
   sealed trait WeatherRequestAction[A]
 
+  case class GetKey(bufferedReader: BufferedReader) extends WeatherRequestAction[String]
   case class GetURL(latitude: Double, longitude: Double, key: String) extends WeatherRequestAction[URI]
 
   class WeatherRequestActions[F[_]](implicit I: InjectK[WeatherRequestAction, F]) {
 
     type WeatherRequestActionF[A] = Free[F, A]
+
+    def getKey(bufferedReader: BufferedReader): WeatherRequestActionF[String] =
+      Free.inject(GetKey(bufferedReader))
 
     def getUrl(latitude: Double, longitude: Double, key: String): WeatherRequestActionF[URI] =
       Free.inject(GetURL(latitude, longitude, key))
