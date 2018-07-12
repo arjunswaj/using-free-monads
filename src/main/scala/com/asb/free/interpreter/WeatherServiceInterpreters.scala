4411651274a3f613911626e300bf65dbed45fa8a
Arjun S Bharadwaj <arjun.s.waj@gmail.com>
2018-07-12 19:42:02 +0530

    Adding GetTemperature in the Interpreter

--- src/main/scala/com/asb/free/interpreter/WeatherServiceInterpreters.scala
+++ src/main/scala/com/asb/free/interpreter/WeatherServiceInterpreters.scala
@@ -5,12 +5,18 @@ import cats.free.Free
 import cats.{Id, ~>}
 import com.asb.free.dsl.FileDSL.{FileAction, FileActions}
 import com.asb.free.dsl.IODSL.{IOAction, IOActions}
-import com.asb.free.dsl.WeatherServiceDSL.{GetKey, WeatherServiceAction}
+import com.asb.free.dsl.NetworkDSL.{NetworkAction, NetworkActions}
+import com.asb.free.dsl.WeatherRequestUtilsDSL.{WeatherRequestAction, WeatherRequestActions}
+import com.asb.free.dsl.WeatherResponseUtilsDSL.{WeatherResponseAction, WeatherResponseActions}
+import com.asb.free.dsl.WeatherServiceDSL.{GetKey, GetTemperature, WeatherServiceAction}
 import com.asb.free.program.WeatherServiceProgram
 
 object WeatherServiceInterpreters {
 
-  type P[A] = EitherK[FileAction, IOAction, A]
+  type P1[A] = EitherK[FileAction, IOAction, A]
+  type P2[A] = EitherK[WeatherRequestAction, P1, A]
+  type P3[A] = EitherK[NetworkAction, P2, A]
+  type P[A] = EitherK[WeatherResponseAction, P3, A]
   type PF[A] = Free[P, A]
 
   val interpreter: (WeatherServiceAction ~> Id) = IntermediateInterpreter andThen PrimitiveInterpreter
@@ -18,14 +24,21 @@ object WeatherServiceInterpreters {
   object IntermediateInterpreter extends (WeatherServiceAction ~> PF) {
     implicit val F: FileActions[P] = FileActions[P]
     implicit val I: IOActions[P] = IOActions[P]
+    implicit val N: NetworkActions[P] = NetworkActions[P]
+    implicit val WRQ: WeatherRequestActions[P] = WeatherRequestActions[P]
+    implicit val WRS: WeatherResponseActions[P] = WeatherResponseActions[P]
 
     override def apply[A](fa: WeatherServiceAction[A]): PF[A] = fa match {
       case GetKey() => WeatherServiceProgram.getKey("resources/weather.key")
+      case GetTemperature(cityResponse, key) => WeatherServiceProgram.getTemperature(cityResponse, key)
     }
   }
 
   object PrimitiveInterpreter extends (PF ~> Id) {
-    val interpreter: P ~> Id = FileInterpreter or IOInterpreter
+    val i1: P1 ~> Id = FileInterpreter or IOInterpreter
+    val i2: P2 ~> Id = WeatherRequestInterpreter or i1
+    val i3: P3 ~> Id = NetworkInterpreter or i2
+    val interpreter: P ~> Id = WeatherResponseInterpreter or i3
 
     override def apply[A](fa: PF[A]): Id[A] = fa.foldMap(interpreter)
   }
--- src/main/scala/com/asb/free/program/WeatherServiceProgram.scala
+++ src/main/scala/com/asb/free/program/WeatherServiceProgram.scala
@@ -2,9 +2,23 @@ package com.asb.free.program
 
 import com.asb.free.dsl.FileDSL.FileActions
 import com.asb.free.dsl.IODSL.IOActions
+import com.asb.free.dsl.NetworkDSL.NetworkActions
+import com.asb.free.dsl.WeatherRequestUtilsDSL.WeatherRequestActions
+import com.asb.free.dsl.WeatherResponseUtilsDSL.WeatherResponseActions
 import com.asb.free.interpreter.WeatherServiceInterpreters.{P, PF}
+import com.maxmind.geoip2.model.CityResponse
 
 object WeatherServiceProgram {
+
+  def getTemperature(cityResponse: CityResponse, key: String)
+                    (implicit WRQ: WeatherRequestActions[P], N: NetworkActions[P],
+                     WRS: WeatherResponseActions[P]): PF[Double] = for {
+    uri <- WRQ.getUrl(cityResponse.getLocation.getLatitude,
+      cityResponse.getLocation.getLongitude, key)
+    response <- N.doGet(uri)
+    temperature <- WRS.getTemperature(response)
+  } yield temperature
+
 
   def getKey(keyPath: String)(implicit F: FileActions[P], I: IOActions[P]): PF[String] = for {
     filePath <- F.getFilePath(keyPath)
